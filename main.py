0fc6a780d967347a1889e1bec3733acf9e904ece
manuelboehm <manuel.boehm@bmw.de>
2023-06-07 21:43:36 +0200

    Add highscore and theme

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.graphics import Rectangle, Color
 from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
+from kivy.storage.jsonstore import JsonStore
+
 import random
 
 
@@ -67,10 +71,13 @@ class Obstacle(Widget):
 class Game(Widget):
     player = Player()
     obstacles = []
+    theme_song = None  # Sound object for the theme song
 
     def __init__(self, **kwargs):
         super(Game, self).__init__(**kwargs)
         self.score = 0
+        self.highscores = []
+        self.store = None
         self.score_label = Label(
             center_x=Window.width / 2, top=Window.height - 20, text="Score: 0"
         )
@@ -84,7 +91,8 @@ class Game(Widget):
 
         if len(self.obstacles) < 1:
             new_obstacle = Obstacle()
-            new_obstacle.velocity = random.randint(10, 30)
+            reinforcement = (self.score / 100 ) + 1
+            new_obstacle.velocity = reinforcement * random.randint(10, 20)
             self.obstacles.append(new_obstacle)
             self.add_widget(self.obstacles[-1])
 
@@ -103,14 +111,70 @@ class Game(Widget):
             if self.player.collide_widget(obstacle):
                 Clock.unschedule(self.update)
                 self.score_label.text = "Game over!"
+                self.theme_song.stop()
+                self.save_highscores()
+                self.show_restart_button()
+                self.show_highscore_label()
 
     def on_touch_down(self, *args):
-        self.player.jump()
+        if self.player.pos[1] == GROUND_HEIGHT:
+            self.player.jump()
+
+    def show_restart_button(self):
+        restart_button = Button(
+            text="Restart", size_hint=(None, None), size=(200, 100),
+            pos=(Window.width / 2 - 100, Window.height / 2 - 50)
+        )
+        restart_button.bind(on_press=self.restart_game)  # Bind on_release event
+        self.parent.add_widget(restart_button)
+
+    def show_highscore_label(self):
+        # Display highscores
+        highscore_label = Label(
+            center_x=Window.width / 2, top=Window.height / 1.5, text="Highscores:\n"
+        )
+        for i, score in enumerate(self.highscores):
+            highscore_label.text += f"{i + 1}. {score}\n"
+        self.add_widget(highscore_label)
+
+    def restart_game(self, instance):
+        self.parent.remove_widget(instance)
+        self.clear_widgets()
+        self.player = Player()
+        self.obstacles = []
+        self.theme_song.play()
+        self.score = 0
+        self.score_label = Label(
+            center_x=Window.width / 2, top=Window.height - 20, text="Score: 0"
+        )
+        self.add_widget(self.score_label)
+        self.add_widget(self.player)
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def load_highscores(self):
+        self.store = JsonStore("highscores.json")
+        if "scores" in self.store:
+            self.highscores = self.store.get("scores")["scores"]
+        else:
+            self.highscores = []
+
+    def save_highscores(self):
+        # Update highscores
+        self.highscores.append(self.score)
+        self.highscores.sort(reverse=True)
+        self.highscores = self.highscores[:5]  # Keep only the top 5 scores
+
+        self.store.put("scores", scores=self.highscores)
 
 
 class MyApp(App):
     def build(self):
         game = Game()
+        game.theme_song = SoundLoader.load('assets/theme.mp3')  # Load the MP3 file
+        if game.theme_song:
+            game.theme_song.loop = True  # Set the theme song to loop
+            game.theme_song.play()  # Start playing the theme song
+        game.load_highscores()
         return game
 
 

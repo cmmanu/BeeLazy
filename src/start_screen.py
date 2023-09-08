7461c027d230236e9b4184fadb8aab08bff5de80
cmmanu <rated-r.superstar@web.de>
2023-09-08 15:04:18 +0200

    align button width on start screen

--- src/start_screen.py
+++ src/start_screen.py
@@ -1,5 +1,5 @@
 """Implements classes of the start screen of the game."""
-
+from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -11,6 +11,8 @@ class StartScreen(Widget):
     This screen appears before the game starts and allows the player to choose whether to start
     the game or show the highscore.
     """
+
+    text_width: int = 0
 
     def __init__(self, start_callback, highscore_callback, back_callback, **kwargs):
         super().__init__(**kwargs)
@@ -44,6 +46,18 @@ class StartScreen(Widget):
 
         self.create_start_screen()
 
+        # update the button width in respect to its text after rendering
+        Clock.schedule_once(self.set_button_width, 0)
+
+    def set_button_width(self, dt):
+        del dt
+        for child in self.children:
+            max_width = max([c.texture_size[0] for c in self.children])
+            if max_width > self.text_width:
+                self.text_width = max_width
+            child.size_hint_x = None
+            child.width = self.text_width + 30
+
     def create_start_screen(self, *args):
         """Creates the start screen on start up."""
 
@@ -67,3 +81,7 @@ class StartScreen(Widget):
         self.remove_widget(self.start_button)
         self.remove_widget(self.highscore_button)
         self.highscore_callback()
+
+        # update the button width in respect to its text after rendering
+        Clock.schedule_once(self.set_button_width, 0)
+
